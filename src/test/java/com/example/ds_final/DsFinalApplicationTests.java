12ee7e8e2cf54ac43879f7ee11f13464c1dabbc6
113306032 <113306032@g.nccu.edu.tw>
2025-12-11 00:56:23 +0800

    initial spring boot project

---
+++ src/test/java/com/example/ds_final/DsFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ds_final;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
